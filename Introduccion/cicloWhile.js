221e058320e912cdab2bd65d61e2a4f6cf335532
ngortiz <nidigortiz21@gmail.com>
2023-07-19 22:22:06 -0400

    Nuevos archivos

---
+++ Introduccion/cicloWhile.js
@@ -0,0 +1,18 @@
+/* Ciclo While
+Es similar a for, pero cona la diferencia de que solo tenemos un condicional.
+Mientras se cumpla el condicional se ejecula el ciclo
+*/
+
+let contador = 0;
+while (contador <= 10) {
+  console.log(contador);
+  contador++;
+}
+/* Ciclo Do While
+ Es similar al ciclo while, con la diferencia de que ejecuta el cloque de codigo al menos una vez
+ */
+let i = 100;
+do {
+  console.log(i);
+  i++;
+} while (i <= 10);
