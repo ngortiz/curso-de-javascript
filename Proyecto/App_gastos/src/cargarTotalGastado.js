29838192f3fbf7168de9e0e2e6fff818f3e03b33
ngortiz <nidigortiz21@gmail.com>
2023-08-25 10:02:16 -0400

    Proyecto AppGastos

---
+++ Proyecto/App_gastos/src/cargarTotalGastado.js
@@ -0,0 +1,30 @@
+import isThisMonth from "date-fns/isThisMonth";
+import { parseISO } from "date-fns";
+
+const cargarTotalGastado = () => {
+  const contenedorTotalGastado = document.getElementById("total-gastado");
+  const gastos = JSON.parse(window.localStorage.getItem("gastos"));
+  let total = 0;
+
+  if (gastos) {
+    const gastosDelMes = gastos.filter((gasto) => {
+      if (isThisMonth(parseISO(gasto.fecha))) {
+        return gasto;
+      }
+    });
+
+    if (gastosDelMes) {
+      gastosDelMes.forEach((gasto) => {
+        total += parseFloat(gasto.precio);
+      });
+    }
+  }
+  // Formateamos el numero y lo agregamos al contenedor.
+  const formatoMoneda = new Intl.NumberFormat("es-PY", {
+    style: "currency",
+    currency: "PYG",
+  });
+  contenedorTotalGastado.innerText = formatoMoneda.format(total);
+};
+
+export default cargarTotalGastado;
---
+++ Proyecto/App_gastos/src/index.js
@@ -0,0 +1,8 @@
+import "./eventoBtnFormularioGasto";
+import "./agregarGasto";
+import cargarGastos from "./cargarGastos";
+import cargarTotalGastado from "./cargarTotalGastado";
+import "./eventosGastos";
+
+cargarGastos();
+cargarTotalGastado();
